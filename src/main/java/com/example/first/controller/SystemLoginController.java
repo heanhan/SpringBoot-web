8c7944ac7de1b511e51e2be76b1d3adc5bbb2e24
zhaojh <1763124707@qq.com>
2019-01-21 20:20:06 +0800

    怎加前端代码功能 ，

---
+++ src/main/java/com/example/first/controller/SystemLoginController.java
@@ -0,0 +1,29 @@
+package com.example.first.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.first.pojo.Users;
+import com.example.first.service.IUserService;
+
+/**
+ *   用户登录信息
+ * @author Thunisoft
+ *
+ */
+@RestController
+public class SystemLoginController {
+	@Autowired
+	private IUserService userServiceImpl;
+	//用户登录信息
+	@RequestMapping(value="/loginInfo")
+	public Users loginInfo(@RequestParam("username") String usercode,@RequestParam("password") String password) 
+	{
+		System.out.println("接收前台的用户信息："+usercode+";"+password);
+		return null;
+	}
+
+}
